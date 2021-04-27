9693a25888d620f2b6f177391224193cbf927eee
Malakhov-cmd <mgs-2000@yandex.ru>
2021-04-27 14:06:12 +0400

    Add user list page without linkage

--- src/main/java/com/example/EventManager/domain/Role.java
+++ src/main/java/com/example/EventManager/domain/Role.java
@@ -3,7 +3,7 @@ package com.example.EventManager.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     //возвращает привязанную роль
     @Override
