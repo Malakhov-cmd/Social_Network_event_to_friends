2f8a665efa0d8cab59d835b3b0a5855cca4302ed
Malakhov-cmd <mgs-2000@yandex.ru>
2021-04-27 14:46:40 +0400

    Add admin rule and access to userList edit page

--- src/main/java/com/example/EventManager/controller/UserController.java
+++ src/main/java/com/example/EventManager/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.example.EventManager.controller;
 
+import com.example.EventManager.domain.Role;
+import com.example.EventManager.domain.User;
 import com.example.EventManager.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+//обеспечиваем доступ только админу
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepo userRepo;
@@ -18,5 +26,40 @@ public class UserController {
     public String userList(Model model){
         model.addAttribute("users", userRepo.findAll());
         return "userList";
+    }
+
+    //помимо /user добавляется id
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user,
+                               Model model)
+    {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam String username,
+                            @RequestParam Map<String, String> form,
+                            @RequestParam("userId") User user)
+    {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for(String key : form.keySet())
+        {
+            if(roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+
+        return "redirect:/user";
     }
 }
