52817e302b18690773894ea6a288aab60ae0b8e3
Malakhov-cmd <mgs-2000@yandex.ru>
2021-04-29 21:02:19 +0400

    try to create dialog system

---
+++ src/main/java/com/example/EventManager/domain/Dialog.java
@@ -0,0 +1,56 @@
+package com.example.EventManager.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Dialog {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private User firstUser;
+    private User secondUser;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "dialog_id")
+    private List<DialogMessage> dialogMessageList;
+
+    public Dialog(User firstUser, User secondUser, List<DialogMessage> dialogMessageList) {
+        this.firstUser = firstUser;
+        this.secondUser = secondUser;
+        this.dialogMessageList = dialogMessageList;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getFirstUser() {
+        return firstUser;
+    }
+
+    public void setFirstUser(User firstUser) {
+        this.firstUser = firstUser;
+    }
+
+    public User getSecondUser() {
+        return secondUser;
+    }
+
+    public void setSecondUser(User secondUser) {
+        this.secondUser = secondUser;
+    }
+
+    public List<DialogMessage> getDialogMessageList() {
+        return dialogMessageList;
+    }
+
+    public void setDialogMessageList(List<DialogMessage> dialogMessageList) {
+        this.dialogMessageList = dialogMessageList;
+    }
+}
---
+++ src/main/java/com/example/EventManager/domain/DialogMessage.java
@@ -0,0 +1,68 @@
+package com.example.EventManager.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class DialogMessage {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private User to;
+    private User from;
+
+    private Date date;
+    private String text;
+
+    public DialogMessage(){}
+
+    public DialogMessage(User to, User from, String text) {
+        this.to = to;
+        this.from = from;
+        this.text = text;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getTo() {
+        return to;
+    }
+
+    public void setTo(User to) {
+        this.to = to;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public void setFrom(User from) {
+        this.from = from;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
