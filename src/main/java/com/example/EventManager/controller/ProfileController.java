c5b3ade8ac1912c61c2fee265769956b34ea3a36
Malakhov-cmd <mgs-2000@yandex.ru>
2021-05-05 16:46:18 +0400

    dab result to relation

--- src/main/java/com/example/EventManager/controller/ProfileController.java
+++ src/main/java/com/example/EventManager/controller/ProfileController.java
@@ -4,6 +4,7 @@ import com.example.EventManager.domain.User;
 import com.example.EventManager.repos.DialogRepo;
 import com.example.EventManager.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,18 +36,24 @@ public class ProfileController {
 
         model.addAttribute("friendList", friendList);
         model.addAttribute("user", user);
+        model.addAttribute("requestFriendCount", user.getFriendRequestList().size());
+        model.addAttribute("respondFriendCount", user.getFriendRespondList().size());
+        model.addAttribute("FriendListSize", user.getFriendList().size());
         return "profile";
     }
 
     @PostMapping("/profile/{user}")
     public String postProfile(@AuthenticationPrincipal User currentUser,
                               @PathVariable("user") User user,
-                              @RequestParam String friendId,
+                              @RequestParam Long friendId,
                               Model model)
     {
-        User futureFriend = userRepo.findByid(Long.getLong(friendId));
+        User futureFriend = userRepo.findByid(friendId);
 
-        if(futureFriend == null)
+        System.out.println("FURURE FRIEND ID: " + futureFriend.getId());
+
+        if(futureFriend.getId() == null
+            || futureFriend.getId() < 1)
         {
             model.addAttribute("friend" , null);
         } else {
@@ -60,8 +67,107 @@ public class ProfileController {
             userRepo.save(futureFriend);
 
             model.addAttribute("friend", futureFriend);
+            model.addAttribute("user", user);
+            model.addAttribute("requestFriendCount", user.getFriendRequestList().size());
+            model.addAttribute("respondFriendCount", user.getFriendRespondList().size());
+            model.addAttribute("FriendListSize", user.getFriendList().size());
         }
 
         return "profile";
+    }
+
+    @GetMapping("/confirmationrequest/{user}")
+    public String getConfirmation( @PathVariable("user") User user,
+                                   Model model)
+    {
+        List<User> userRequest = user.getFriendRequestList();
+
+        model.addAttribute("futureFriendRequest", userRequest);
+
+        return "confirmationFriendRequest";
+    }
+
+    @PostMapping("/confirmationrequest/{user}")
+    public String postConfirmation(@PathVariable("user") User user,
+                                   @RequestParam String design,
+                                   @RequestParam Long secondUser,
+                                   Model model)
+    {
+        User potentialFriend = userRepo.findByid(secondUser);
+
+        switch (design)
+        {
+            case("add"):
+                List<User> userFriend = user.getFriendList();
+                userFriend.add(potentialFriend);
+
+                user.setFriendList(userFriend);
+
+                List<User> userRequest = user.getFriendRequestList();
+                List<User> potentialFriendRespond = potentialFriend.getFriendRespondList();
+                if(userRequest.contains(potentialFriend))
+                {
+                    userRequest.remove(potentialFriend);
+                    user.setFriendRequestList(userRequest);
+                    potentialFriendRespond.remove(user);
+                    potentialFriend.setFriendRespondList(potentialFriendRespond);
+                }
+
+                List<User> userRespond = user.getFriendRespondList();
+                List<User> potentialRequest = potentialFriend.getFriendRequestList();
+                if (userRespond.contains(potentialFriend))
+                {
+                    userRespond.remove(potentialFriend);
+                    user.setFriendRespondList(userRespond);
+                    potentialRequest.remove(user);
+                    potentialFriend.setFriendRequestList(potentialRequest);
+                }
+                userRepo.save(user);
+                userRepo.save(potentialFriend);
+            break;
+            case ("reject"):
+                userRequest = user.getFriendRequestList();
+                potentialFriendRespond = potentialFriend.getFriendRespondList();
+
+                if(userRequest.contains(potentialFriend))
+                {
+                    userRequest.remove(potentialFriend);
+                    user.setFriendRequestList(userRequest);
+                    potentialFriendRespond.remove(user);
+                    potentialFriend.setFriendRespondList(potentialFriendRespond);
+                }
+
+                userRespond = user.getFriendRespondList();
+                potentialRequest = potentialFriend.getFriendRequestList();
+                if (userRespond.contains(potentialFriend))
+                {
+                    userRespond.remove(potentialFriend);
+                    user.setFriendRespondList(userRespond);
+                    potentialRequest.remove(user);
+                    potentialFriend.setFriendRequestList(potentialRequest);
+                }
+                userRepo.save(user);
+                userRepo.save(potentialFriend);
+            break;
+            case ("noting"):
+                break;
+        }
+
+        List<User> userRequest = user.getFriendRequestList();
+
+        model.addAttribute("futureFriendRequest", userRequest);
+
+        return "confirmationFriendRequest";
+    }
+
+    @GetMapping("/respondlist/{user}")
+    public String getRespond(@PathVariable("user") User user,
+                             Model model)
+    {
+        List<User> userRespond = user.getFriendRespondList();
+
+        model.addAttribute("userRespond", userRespond);
+
+        return "respond";
     }
 }
